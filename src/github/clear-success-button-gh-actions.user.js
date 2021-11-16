3eea0c669b72348560235d77414875ae74cabdd3
eranelbaz <eranelbaz97@gmail.com>
2021-11-16 23:18:55 +0200

    path

--- src/github/clear-success-button-gh-actions.user.js
+++ src/github/clear-success-button-gh-actions.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         Add clear successful button on gh actions
 // @namespace    github
-// @version      0.3.1
+// @version      0.3.2
 // @description  Add clear successful button on gh actions
 // @author       You
 // @include      https://github.com/*
 // @icon         https://github.githubassets.com/favicons/favicon.png
 // @grant        none
 // @require      https://raw.githubusercontent.com/eranelbaz/userscripts/main/src/mutations.js
-// @updateURL    https://raw.githubusercontent.com/eranelbaz/userscripts/main/github/clear-success-button-gh-actions.user.js
-// @downloadURL  https://raw.githubusercontent.com/eranelbaz/userscripts/main/github/clear-success-button-gh-actions.user.js
+// @updateURL    https://raw.githubusercontent.com/eranelbaz/userscripts/main/src/github/clear-success-button-gh-actions.user.js
+// @downloadURL  https://raw.githubusercontent.com/eranelbaz/userscripts/main/src/github/clear-success-button-gh-actions.user.js
 // ==/UserScript==
 
 (function() {
