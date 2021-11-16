3476b919f99e30029e428e83e095d628004e3071
eranelbaz <eranelbaz97@gmail.com>
2021-11-16 23:22:59 +0200

    check if already exists

--- src/github/clear-success-button-gh-actions.user.js
+++ src/github/clear-success-button-gh-actions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Add clear successful button on gh actions
 // @namespace    github
-// @version      0.3.2
+// @version      0.3.3
 // @description  Add clear successful button on gh actions
 // @author       You
 // @include      https://github.com/*
