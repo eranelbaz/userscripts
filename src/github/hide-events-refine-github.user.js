e9a3f2753a29c5c6b3b4536f07971c9558b9acce
eranelbaz <eranelbaz97@gmail.com>
2021-11-16 23:16:11 +0200

    Debounce (#3)

--- src/github/hide-events-refine-github.user.js
+++ src/github/hide-events-refine-github.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hide events in github (refine github)
 // @namespace    github
-// @version      0.3
+// @version      0.3.1
 // @author       You
 // @include      https://github.com/*
 // @icon         https://github.githubassets.com/favicons/favicon.png
@@ -24,4 +24,5 @@
     }
 
     document.addEventListener("ghmo:comments", init);
+    document.addEventListener("ghmo:comments", () => setTimeout(init, 500));
 })();
