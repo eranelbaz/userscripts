dea84649fed2f3abed8d14070fd8cf8bb303ec93
eranelbaz <eranelbaz97@gmail.com>
2021-11-19 12:08:29 +0200

    remove from function (#23)

--- src/helpers.js
+++ src/helpers.js
@@ -1,16 +1,14 @@
-(() => {
-    "use strict";
+"use strict";
 
-    const bindForElementChange = (targets) => {
-        const targetList = Object.keys(targets);
-        ["DOMNodeInserted", "DOMNodeRemoved", "DOMAttributeNameChanged"].forEach(eventName => {
-            document.addEventListener(eventName, (e) => {
-                if (targetList.includes(e.target.id) && targets[targetList].type === 'id') {
-                    const event = new Event(targets[targetList].eventName);
-                    document.dispatchEvent(event);
-                }
+const bindForElementChange = (targets) => {
+    const targetList = Object.keys(targets);
+    ["DOMNodeInserted", "DOMNodeRemoved", "DOMAttributeNameChanged"].forEach(eventName => {
+        document.addEventListener(eventName, (e) => {
+            if (targetList.includes(e.target.id) && targets[targetList].type === 'id') {
+                const event = new Event(targets[targetList].eventName);
+                document.dispatchEvent(event);
+            }
 
-            }, false);
-        });
-    }
-})();
+        }, false);
+    });
+}
