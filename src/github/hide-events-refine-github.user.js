73e5b67af6f49567d2b21173bd0d36ad025de54c
Elbaz <eranelbaz97+github@gmail.com>
2023-05-23 12:35:10 +0300

    use on element ready

--- src/github/hide-events-refine-github.user.js
+++ src/github/hide-events-refine-github.user.js
@@ -1,27 +1,26 @@
 // ==UserScript==
 // @name         Hide events in github (refine github)
 // @namespace    github
-// @version      0.3.7
+// @version      0.3.8
 // @author       You
 // @include      https://github.com/*
 // @icon         https://github.githubassets.com/pinned-octocat.svg
 // @grant        none
-// @require      https://raw.githubusercontent.com/eranelbaz/userscripts/main/src/github/mutations.js
-// @require      https://raw.githubusercontent.com/eranelbaz/userscripts/main/src/github/init.js
 // @require      https://raw.githubusercontent.com/eranelbaz/userscripts/main/src/github/url-detection.js
 // @updateURL    https://raw.githubusercontent.com/eranelbaz/userscripts/main/src/github/hide-events-refine-github.user.js
 // @downloadURL  https://raw.githubusercontent.com/eranelbaz/userscripts/main/src/github/hide-events-refine-github.user.js
-// ==/UserScript==
-(function() {
-    'use strict';
+// @require      https://greasyfork.org/scripts/419640-onelementready/code/onElementReady.js?version=887637
 
-    const init  = () => {
-        if(isPR()) {
-            var label = document.querySelector('[data-value="hideEvents"]');
-            if(label) {
-                label.click();
-            }
-        }
+// ==/UserScript==
+
+const init = () => {
+  if (isPR()) {
+    var label = document.querySelector('[data-value="hideEvents"]');
+    if (label) {
+      label.click();
     }
-    bindInit(["ghmo:container","ghmo:comments"], init)
-})();
+  }
+};
+
+
+onElementReady(`#rgh-conversation-activity-filter-select-menu`, { findOnce: true }, init);
